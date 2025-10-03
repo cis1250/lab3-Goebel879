6174f2d18e9fd990f031c3a014af498ab7225afa
Leo Goebel <goebell@uoguelph.ca>
2025-10-03 12:24:42 -0400

    Improve Fibonacci sequence user input and output
    Refactor Fibonacci sequence input handling and printing logic.

--- fibonacci.py
+++ fibonacci.py
@@ -3,11 +3,27 @@
 # Fibonacci Sequence Exercise
 # TODO: (Read detailed instructions in the Readme file)
 # Prompt the user for the number of terms.
-print("User input: ")
-input()
-if (input > 0)
- print("0,1,1,2,3,3,5)
-else
-  print("Please enter a positive integer.")
 # Validate that the input is a positive integer.
 # Use a for loop to print the Fibonacci sequence up to that many terms.
+input_user1 = 0
+while (input_user1 <= 0):
+    input_user = input("User input: ")
+    input_user1 = int(input_user)
+    forL = input_user1//2
+    if(input_user1 > 0):
+        x1 = 0
+        x2 = 1
+        x = forL
+        if(input_user1%2 == 0):
+            for x in range(forL):
+                print(x1,x2,end = " ")
+                x1+= x2
+                x2+= x1
+        else:
+            for x in range(forL):
+                print(x1,x2,end = " ")
+                x1+= x2
+                x2+= x1
+            print(x1,end = " ")
+    else:
+        print("Please enter a positive integer.")
