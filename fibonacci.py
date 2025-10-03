8db1b09b1560307656b06f6268b0b390f0ec2787
Leo Goebel <goebell@uoguelph.ca>
2025-10-03 11:38:49 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -3,5 +3,11 @@
 # Fibonacci Sequence Exercise
 # TODO: (Read detailed instructions in the Readme file)
 # Prompt the user for the number of terms.
+print("User input: ")
+input()
+if (input > 0)
+ print("0,1,1,2,3,3,5)
+else
+  print("Please enter a positive integer.")
 # Validate that the input is a positive integer.
 # Use a for loop to print the Fibonacci sequence up to that many terms.
